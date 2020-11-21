f1c2143bbe5789536491ff73ea0f17d31858265f
Boro <mikolajborowicz7@gmail.com>
2020-11-21 14:56:04 +0100

    zarys gui w tkinter

---
+++ tkniter/gui_tkniter_v1.py
@@ -0,0 +1,19 @@
+from tkinter import * #don't judge me for using * ;)
+
+def start_gry():
+    info = Label(root, text="rozpoczynanie gry")
+    info.grid()
+
+root = Tk()
+
+#tytuł 
+tutul = Label(root, text="Dobblethon")
+tutul.grid(row=1, column=5) #the column is more like presedence 
+
+# przyciski
+PrzyciskStart = Button(root, text="Rozpocznij Grę", command=start_gry)
+PrzyciskStart.grid(row=2,column=5,padx=100,pady=50)
+
+root.mainloop() #wyświetlanie okna
+
+
